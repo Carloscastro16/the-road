64d0bca58254b3cd68a3f39b2325704303823b7a
Angel-Maximiliano <angelbacab001@hotmail.com>
2024-06-11 15:13:18 -0500

    up: Se ajustó el diseño y se agregó la propiedad responsive a las tarjetas

--- src/Components/Models/Landing/OfferSection.tsx
+++ src/Components/Models/Landing/OfferSection.tsx
@@ -3,13 +3,45 @@ import { Box, Button, Grid, Typography, Paper } from '@mui/material';
 import { styled } from '@mui/system';
 import Img from '../../../assets/Img/Codigo.png';
 
-const ImageContainer = styled('div')({
+const ImageContainer = styled('div')(({ theme }) => ({
   backgroundImage: `url(${Img})`, // Ajusta la ruta de tu imagen
   backgroundSize: 'cover',
   backgroundPosition: 'center',
-  height: 'auto', // Ajusta la altura automáticamente
-  flex: '0.7 0 200px', // Controla el tamaño de la imagen
-});
+  height: '200px', // Ajusta la altura
+  [theme.breakpoints.up('md')]: {
+    flex: '0.7 0 200px', // Controla el tamaño de la imagen en pantallas grandes
+    height: 'auto', // Ajusta la altura automáticamente en pantallas grandes
+  },
+}));
+
+const ContentContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: theme.spacing(3, 5),
+  [theme.breakpoints.up('md')]: {
+    flex: 2,
+    alignItems: 'flex-start',
+  },
+}));
+
+const CustomButton = styled(Button)(({ theme }) => ({
+  backgroundColor: '#419593',
+  color: 'white',
+  marginBottom: theme.spacing(3),
+  width: 'fit-content',
+  alignSelf: 'center',
+  [theme.breakpoints.up('md')]: {
+    alignSelf: 'flex-start',
+  },
+}));
+
+const AlignLeftTypography = styled(Typography)(({ theme }) => ({
+  textAlign: 'center',
+  [theme.breakpoints.up('md')]: {
+    textAlign: 'left',
+  },
+}));
 
 const Ofrecemos = () => {
   return (
@@ -18,52 +50,81 @@ const Ofrecemos = () => {
         ¿Qué ofrecemos?
       </Typography>
 
-      <Paper elevation={3} sx={{ display: 'flex', mb: 4,borderRadius:'10px' }}>
-        <Box sx={{ flex: 2, p: 3, pr: 5, pl:5}}> {/* Ajusta el padding para dejar un margen derecho */}
+      <Paper
+        elevation={3}
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column-reverse', md: 'row' },
+          mb: 4,
+          borderRadius: '10px',
+          backgroundColor: 'rgba(145, 139, 198, 0.30)',
+        }}
+      >
+        <ContentContainer>
           <Typography variant="h5" gutterBottom sx={{ mb: 3 }}>
             Afila tus habilidades de codificación
           </Typography>
-          <Typography variant="body1" gutterBottom sx={{ mb: 3, pr:12}}>
+          <Typography variant="body1" gutterBottom sx={{ mb: 3, pr: { md: 12 } }}>
             Lleva tus conocimientos al siguiente nivel enfrentándote a desafíos continuos. Mejora tu lógica, optimiza tus soluciones y conviértete en un desarrollador más eficiente y seguro. ¡Empieza hoy y ve el progreso en cada línea de código!
           </Typography>
-          <Button variant="contained" color="primary" sx={{ mb: 3 }}>
+          <CustomButton>
             Unirme
-          </Button>
-        </Box>
+          </CustomButton>
+        </ContentContainer>
         <ImageContainer />
       </Paper>
 
       <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <Paper elevation={3} sx={{ p: 3 }}>
+        <Grid item xs={12} md={6} sx={{ mb: { xs: 4, md: 0 } }}>
+          <Paper
+            elevation={3}
+            sx={{
+              p: 3,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              height: '100%',
+              alignItems: { xs: 'center', md: 'flex-start' },
+            }}
+          >
             <Typography variant="h5" gutterBottom>
               Mantente actualizado
             </Typography>
             <Typography variant="body1" gutterBottom>
               Conoce las tendencias más recientes, explora nuevas herramientas y mejora tus habilidades para seguir siendo competitivo en el campo tecnológico.
             </Typography>
-            <Typography variant="body1" gutterBottom>
+            <AlignLeftTypography variant="body1" gutterBottom sx={{ fontWeight: 'bold' }}>
               ¡La innovación no espera, y tú tampoco deberías hacerlo!
-            </Typography>
-            <Button variant="outlined" color="primary">
+            </AlignLeftTypography>
+            <CustomButton>
               IMAGEN ILUSTRATIVA
-            </Button>
+            </CustomButton>
           </Paper>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Paper elevation={3} sx={{ p: 3 }}>
+          <Paper
+            elevation={3}
+            sx={{
+              p: 3,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              height: '100%',
+              alignItems: { xs: 'center', md: 'flex-start' },
+            }}
+          >
             <Typography variant="h5" gutterBottom>
               Gana puntos y compite con tus amigos
             </Typography>
             <Typography variant="body1" gutterBottom>
               Cada vez que resuelvas una actividad, ganarás puntos que te ayudarán a escalar en nuestro ranking general. Desafía a tus amigos y a la comunidad mientras mejoras tus habilidades de programación.
             </Typography>
-            <Typography variant="body1" gutterBottom>
+            <AlignLeftTypography variant="body1" gutterBottom sx={{ fontWeight: 'bold' }}>
               ¡Empieza ahora y alcanza la cima!
-            </Typography>
-            <Button variant="outlined" color="primary">
+            </AlignLeftTypography>
+            <CustomButton>
               IMAGEN ILUSTRATIVA
-            </Button>
+            </CustomButton>
           </Paper>
         </Grid>
       </Grid>
