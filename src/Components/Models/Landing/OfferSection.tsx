8008e7b8ed32a46db2190ece9be0696941b1b884
Carloscastro16 <andycastro.001016@gmail.com>
2024-06-17 12:57:11 -0500

    Banner changes

--- src/Components/Models/Landing/OfferSection.tsx
+++ src/Components/Models/Landing/OfferSection.tsx
@@ -7,13 +7,13 @@ import Img3 from '../../../assets/images/Vector2.png';
 
 
 const ImageContainer = styled('div')(({ theme }) => ({
-  backgroundImage: `url(${Img})`, 
+  backgroundImage: `url(${Img})`,
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   height: '200px',
   [theme.breakpoints.up('md')]: {
-    flex: '0.7 0 200px', 
-    height: 'auto', 
+    flex: '0.7 0 200px',
+    height: 'auto',
   },
 }));
 
@@ -48,39 +48,38 @@ const AlignLeftTypography = styled(Typography)(({ theme }) => ({
 
 const Ofrecemos = () => {
   return (
-    <Box sx={{ padding: 11 }}>
+    <Box sx={{ paddingInline: '42px', marginBottom: '42px' }}>
       <Typography variant="h3" align="center" gutterBottom>
         ¿Qué ofrecemos?
       </Typography>
 
-      <Paper
-        elevation={3}
-        sx={{
-          display: 'flex',
-          flexDirection: { xs: 'column-reverse', md: 'row' },
-          mb: 4,
-          borderRadius: '10px',
-          backgroundColor: 'rgba(145, 139, 198, 0.30)',
-        
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={12}>
+          <Paper
+            elevation={3}
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column-reverse', md: 'row' },
+              borderRadius: '10px',
+              backgroundColor: 'rgba(145, 139, 198, 0.30)',
+            }}
+          >
+            <ContentContainer>
+              <Typography variant="h5" gutterBottom sx={{ mb: 3, fontWeight: 'bold' }}>
+                Afila tus habilidades de codificación
+              </Typography>
+              <Typography variant="body1" gutterBottom sx={{ mb: 3, pr: { md: 12 } }}>
+                Lleva tus conocimientos al siguiente nivel enfrentándote a desafíos continuos. Mejora tu lógica, optimiza tus soluciones y conviértete en un desarrollador más eficiente y seguro. ¡Empieza hoy y ve el progreso en cada línea de código!
+              </Typography>
+              <CustomButton>
+                Unirme
+              </CustomButton>
+            </ContentContainer>
+            <ImageContainer />
+          </Paper>
 
-        }}
-      >
-        <ContentContainer>
-          <Typography variant="h5" gutterBottom sx={{ mb: 3 ,fontWeight: 'bold' }}>
-            Afila tus habilidades de codificación
-          </Typography>
-          <Typography variant="body1" gutterBottom sx={{ mb: 3, pr: { md: 12 } }}>
-            Lleva tus conocimientos al siguiente nivel enfrentándote a desafíos continuos. Mejora tu lógica, optimiza tus soluciones y conviértete en un desarrollador más eficiente y seguro. ¡Empieza hoy y ve el progreso en cada línea de código!
-          </Typography>
-          <CustomButton>
-            Unirme
-          </CustomButton>
-        </ContentContainer>
-        <ImageContainer />
-      </Paper>
-
-      <Grid container spacing={5}>
-        <Grid item xs={12} md={5} sx={{ mb: { xs: 6, md: 0 } }}>
+        </Grid>
+        <Grid item xs={4} md={4} sx={{ mb: { xs: 6, md: 0 } }}>
           <Paper
             elevation={3}
             sx={{
@@ -90,7 +89,7 @@ const Ofrecemos = () => {
               justifyContent: 'space-between',
               height: '100%',
               alignItems: { xs: 'center', md: 'flex-start' },
-              borderRadius: '20px', 
+              borderRadius: '20px',
               background: '#b1ded951',
               backdropFilter: 'blur(40px)'
 
@@ -109,7 +108,7 @@ const Ofrecemos = () => {
 
           </Paper>
         </Grid>
-        <Grid item xs={12} md={7}>
+        <Grid item xs={4} md={7}>
           <Paper
             elevation={3}
             sx={{
@@ -119,12 +118,12 @@ const Ofrecemos = () => {
               justifyContent: 'space-between',
               height: '100%',
               alignItems: { xs: 'center', md: 'flex-start' },
-              borderRadius: '20px', 
+              borderRadius: '20px',
               background: '#b1ded951',
               backdropFilter: 'blur(40px)'
             }}
           >
-             <img src={Img3} alt="Icono" style={{ width: '50px', marginBottom: '16px' }} />
+            <img src={Img3} alt="Icono" style={{ width: '50px', marginBottom: '16px' }} />
             <Typography variant="h5" gutterBottom sx={{ fontWeight: 'bold' }}>
               Gana puntos y compite con tus amigos
             </Typography>
