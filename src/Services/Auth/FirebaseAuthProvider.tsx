400d8da9332e2917eccc03d3adfa2453c188beae
Carloscastro16 <andycastro.001016@gmail.com>
2024-07-13 10:33:39 -0500

    firebase implementation

--- src/Services/Auth/FirebaseAuthProvider.tsx
+++ src/Services/Auth/FirebaseAuthProvider.tsx
@@ -1,7 +1,25 @@
 import React, { createContext, useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import auth from "../../../firebaseConfig"; // Asegúrate de importar auth correctamente desde FirebaseConfig
+import PropTypes from "prop-types"; // Asegúrate de importar auth correctamente desde FirebaseConfig
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, UserCredential, User } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrtXVclBpiiejuIRd_GUC1M1iS3IIQvfY",
+  authDomain: "the-road-coding.firebaseapp.com",
+  projectId: "the-road-coding",
+  storageBucket: "the-road-coding.appspot.com",
+  messagingSenderId: "880847463846",
+  appId: "1:880847463846:web:5f7827a55e93224a0f1b67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 interface AuthContextProps {
   createUser: (email: string, password: string) => Promise<UserCredential>;
